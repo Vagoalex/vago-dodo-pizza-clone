68a575609b0871d2d28dc351ccc690689e94105d
Aleksandr Vago <vagoalex666@mail.ru>
2024-09-07 00:04:21 +0400

    feat: added many features

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,33 @@
-import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
-import "./globals.css";
-import React from "react";
+import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
+import './globals.css'
+import React from 'react'
+import { Header } from '@/components/shared/header/header'
 
 const nunitoFont = Nunito({
-	subsets: ["cyrillic"],
-	variable: "--font-nunito",
-	weight: ["400", "500", "600", "700", "800", "900"]
-});
+    subsets: ['cyrillic'],
+    variable: '--font-nunito',
+    weight: ['400', '500', '600', '700', '800', '900'],
+})
 
 export const metadata: Metadata = {
-	title: "Vago Dodo pizza clone | Главная",
-	description: "Dodo pizza clone with nest.js and react + prisma"
-};
+    title: 'Vago Dodo pizza clone | Главная',
+    description: 'Dodo pizza clone with nest.js and react + prisma',
+}
 
 export default function RootLayout({
-	children
+    children,
 }: Readonly<{
-	children: React.ReactNode;
-}>)
-{
-	return (
-		<html lang="en">
+    children: React.ReactNode
+}>) {
+    return (
+        <html lang="en">
             <body className={nunitoFont.variable}>
-                <main className="min-h-screen">{children}</main>
+                <main className="min-h-screen">
+                    <Header />
+                    {children}
+                </main>
             </body>
-		</html>
-	);
+        </html>
+    )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
-  return (
-    <div>
-      <main>
-          <div className="flex m-2">
-            <Button variant="outline">Кликни на меня</Button>
-          </div>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <main>
+                <div className="flex m-2">
+                    <Button variant="outline">Кликни на меня</Button>
+                </div>
+            </main>
+        </div>
+    )
 }
