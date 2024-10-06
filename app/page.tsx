c6d96995ca5ce431717499ed7fa4f9d12d19cef1
Aleksandr Vago <vagoalex666@mail.ru>
2024-10-06 15:16:19 +0300

    feat: added types for items in checkbox-filters-group

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { Container, TopBar } from '@/components/shared';
 import { Title } from '@/components/shared/typography';
 import { Filters } from '@/components/shared/filters';
 
-// TODO: остановился на 1:18:52
 export default function Home() {
     return (
         <>
--- components/shared/fields/checkbox-filters-group.tsx
+++ components/shared/fields/checkbox-filters-group.tsx
@@ -1,17 +1,29 @@
 import { cn } from '@/lib/utils';
 import React from 'react';
+import { FilterCheckboxProps } from '@/components/shared/fields/filter-checkbox';
+
+type Item = FilterCheckboxProps;
 
 interface Props {
     title: string;
     className?: string;
-    items: unknown[];
+    items: Item[];
     limit?: number;
     searchInputPlaceholder?: string;
     onChange?: (values: string[]) => void;
-    defaultValue?: string[];
+    defaultValue?: Item[];
     defaultItems?: unknown[];
 }
 
-export const CheckboxFiltersGroup: React.FC<Props> = ({ title, className }) => {
+export const CheckboxFiltersGroup: React.FC<Props> = ({
+    title,
+    className,
+    items,
+    limit = 5,
+    searchInputPlaceholder = 'Поиск...',
+    onChange,
+    defaultValue,
+    defaultItems,
+}) => {
     return <div className={cn('mx-auto max-w-[1280px]', className)}></div>;
 };
